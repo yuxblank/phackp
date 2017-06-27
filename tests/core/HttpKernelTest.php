9d6fee84050b678417e2c7aec911707be9c401f7
yuri.blanc <yuxblank@gmail.com>
2017-06-27 16:49:13 +0200

    unit test update

---
+++ tests/core/HttpKernelTest.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: yuri.blanc
+ * Date: 27/06/2017
+ * Time: 16:40
+ */
+class HttpKernelTest
+{
+
+}
---
+++ tests/database/DatabaseTest.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: yuri.blanc
+ * Date: 26/06/2017
+ * Time: 14:48
+ */
+class DatabaseTest
+{
+
+}
