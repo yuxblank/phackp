c46a64ad02dcf51e5fdee6ce863e7308ebd79990
Yuri Blanc <yuxblank@gmail.com>
2018-03-03 17:56:11 +0100

    Handle doctrine close

--- src/phackp/core/ErrorHandler.php
+++ src/phackp/core/ErrorHandler.php
@@ -10,6 +10,10 @@ namespace yuxblank\phackp\core;
 
 
 use DI\Container;
+use DI\DependencyException;
+use DI\NotFoundException;
+use Doctrine\DBAL\Exception\ConnectionException;
+use Doctrine\ORM\EntityManagerInterface;
 use yuxblank\phackp\core\api\ApplicationController;
 use yuxblank\phackp\core\api\ErrorHandlerInterface;
 use yuxblank\phackp\core\api\LifeCycleInterface;
@@ -55,9 +59,24 @@ class ErrorHandler implements ErrorHandlerInterface
 
     public function exceptionHandler(\Throwable $exception)
     {
+        $this->handleEntityManagerStatus();
         $this->container->set(ApplicationController::class, $this->error500route->getClass());
         $this->lifeCycle->callController($this->error500route->getAction(), $exception);
     }
 
+    private function handleEntityManagerStatus()
+    {
+        try {
+            /** @var EntityManagerInterface $doctrineDriver */
+            $doctrineDriver = $this->container->get(EntityManagerInterface::class);
+            $doctrineDriver->rollback();
+        } catch (\Exception $exception) {
+
+        }
+        if ($doctrineDriver) {
+            $doctrineDriver->close();
+        }
+
+    }
 
 }
